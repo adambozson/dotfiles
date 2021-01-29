e190583931f6d1d77507dea7f02c515ca8a2d231
Adam Bozson <adam@bozson.io>
2021-01-29 17:18:03 +0000

    Config for M1

--- fish/config.fish
+++ fish/config.fish
@@ -1,1 +1,6 @@
-set -g fish_user_paths "/usr/local/sbin" $fish_user_paths
+if test (arch) = "arm64"
+    eval (/opt/homebrew/bin/brew shellenv)
+else
+    set -g fish_user_paths "/usr/local/sbin" $fish_user_paths
+end
+
