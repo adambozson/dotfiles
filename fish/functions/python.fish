79856fa887d932d68983a0b63455abe5c3beb6ea
Adam Bozson <adam@bozson.io>
2021-06-07 08:56:43 +0100

    pass args to python

--- fish/functions/python.fish
+++ fish/functions/python.fish
@@ -1,7 +1,7 @@
 function python --description 'Use system python3 outside of conda'
     if set -q CONDA_PREFIX
-        command python
+        command python $argv
     else
-        command python3
+        command python3 $argv
     end
 end
