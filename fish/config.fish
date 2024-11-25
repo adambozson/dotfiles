46753acfe589baa7b44c5f821f4da9650d127624
Adam Bozson <adam@bozson.io>
2024-11-25 10:15:48 +0000

    no longer need conda/gcp

--- fish/config.fish
+++ fish/config.fish
@@ -5,20 +5,4 @@ if status is-interactive
     
     # disable default fish welcome messasge
     set -g fish_greeting
-    
-    if test (arch) = "arm64"
-        set -g HOMEBREW_PREFIX /opt/homebrew
-        set -g CONDA_PREFIX $HOMEBREW_PREFIX/anaconda3
-    else
-        set -g HOMEBREW_PREFIX /usr/local
-        set -g CONDA_PREFIX ~/anaconda3
-    end
-
-    eval ($HOMEBREW_PREFIX/bin/brew shellenv)
-
-    # If google-cloud-sdk is installed, run its include script
-    set -l gc_dir $HOMEBREW_PREFIX/Caskroom/google-cloud-sdk
-    if test -e $gc_dir
-        source "$gc_dir/latest/google-cloud-sdk/path.fish.inc"
-    end
 end
